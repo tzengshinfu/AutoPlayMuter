bd106327104672ec8f3180469f7f4f4d764c2f0e
Frank_Tseng <frank_tseng@prologium.com.tw>
2017-04-26 12:16:53 +0800

    v1.2.0 Release Notes: -Change enable sound function shortcut(mouse click→double click)

--- Source/content_scripts.js
+++ Source/content_scripts.js
@@ -1,16 +1,8 @@
 function checkDOM() {
-	//for normal videos
-	window.addEventListener("click", function () {
-		chrome.runtime.sendMessage({ clicking: true });
-	});
-
-	//for iframe videos (Thanks to jaydson<https://gist.github.com/jaydson/1780598>)
-	window.addEventListener("mouseover", function () {
-		window.focus();
-	});
-	window.addEventListener("blur", function () {
+	window.addEventListener("dblclick", function () {
 		chrome.runtime.sendMessage({ clicking: true });
 	});
 }
+
 
 checkDOM();
