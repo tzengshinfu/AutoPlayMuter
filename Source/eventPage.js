00523ad6afcefc45844720fbc98884b867959762
tzengshinfu <tzengshinfu@gmail.com>
2017-04-26 23:36:30 +0800

    v1.2.1 Bug fixes:fix bug of white list function.

--- Source/eventPage.js
+++ Source/eventPage.js
@@ -2,18 +2,18 @@ var tabLists = {};
 var whiteLists = ["youtube.com", "netflix.com", "vimeo.com", "screen.yahoo.com", "dailymotion.com", "hulu.com", "vube.com", "twitch.tv", "liveleak.com", "vine.co", "ustream.tv", "break.com", "tv.com", "metacafe.com", "viewster.com", "vevo.com", "viacom.com", "video.aol.com"];
 
 function muteTab(tab) {
-  let isMuted = true;
   for (whiteList of whiteLists) {
     if (tab.url.includes(whiteList)) {
-        return;
+      chrome.tabs.update(tab.id, { muted: false });
+      return;
     }
-  }    
-    
+  }
+
   if (typeof tabLists[tab.id] === "undefined") {
-    chrome.tabs.update(tab.id, { muted: isMuted });
+    chrome.tabs.update(tab.id, { muted: true });
   }
   else if (tabLists[tab.id] !== true) {
-    chrome.tabs.update(tab.id, { muted: isMuted });
+    chrome.tabs.update(tab.id, { muted: true });
   }
   else {
     tabLists[tab.id] = false;
